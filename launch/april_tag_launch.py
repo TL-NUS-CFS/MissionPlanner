131d28e5a67b9ae8286a0df152bfe12b6bfde5a9
swarm <liewyungjun@gmail.com>
2023-10-24 13:28:52 +0800

    launch files

---
+++ launch/april_tag_launch.py
@@ -0,0 +1,47 @@
+'''
+"ros2 run ai_deck_wrapper ai_deck_wrapper --ros-args -p period:=0.1 -r image_rect:=/cf13/image_rect -r camera_info:=/cf13/camera_info "
+"ros2 run apriltag_ros apriltag_node --ros-args -r image_rect:=/cf13/image_rect -r tf:=/cf13/tf -r camera_info:=/cf13/camera_info -r detections:=/cf13/detections"
+"ros2 run mission_planner mission_planner"
+'''
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch_ros.actions import Node
+
+
+
+
+
+def generate_launch_description():
+    ai_deck_wrapper = Node(
+            package='ai_deck_wrapper',
+            executable='ai_deck_wrapper',
+            namespace='cf13',
+            output='screen',
+            parameters=[{
+                'period': 0.1,
+            }],
+            remappings=[
+                ('/image_rect', '/cf13/image_rect'),
+                ('/camera_info', '/cf13/camera_info'),
+            ])
+    apriltag_node = Node(
+            package='apriltag_ros',
+            executable='apriltag_node',
+            namespace='cf13',
+            output='screen',
+            remappings=[
+                ('/image_rect', '/cf13/image_rect'),
+                ('/camera_info', '/cf13/camera_info'),
+                ('/tf','/cf13/tf'),
+                ('/detections','/cf13/detections'),
+            ]
+            )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    ld.add_action(ai_deck_wrapper)
+    ld.add_action(apriltag_node)
+
+    return ld
