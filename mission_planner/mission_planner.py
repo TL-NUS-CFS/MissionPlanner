1aab149f980b8f0cdb7d4587787e27675f938734
swarm <liewyungjun@gmail.com>
2024-01-18 17:24:28 +0800

    resolve conflicts

--- mission_planner/mission_planner.py
+++ mission_planner/mission_planner.py
@@ -24,11 +24,8 @@ class MissionPlanner(Node):
     def __init__(self):
         
         super().__init__('mission_planner')
-        #maybe do a subscription creation loop for drone in drones[]
-        #self.declare_parameter("undetectedTags", ["tag36h11:52","tag36h11:203"]) 
-        #self.undetectedTags = set(self.get_parameter("undetectedTags"))
         self.undetectedTags = {"tag36h11:200","tag36h11:204"}
-        drones = ["cf37","cf38","cf39"]
+        drones = ["cf37","cf38","cf39", "cf16","cf36"]
         self.callbacks = {}
    
             
@@ -50,75 +47,8 @@ class MissionPlanner(Node):
                 drone + '/tf',
                 self.callbacks[drone],
                 10)
-            #self.subscriptions.append(current_subscription)
             current_subscription
  
-
-
-
-
-    ###############   TEDIOUS METHOD #######################
-
-    # def __init__(self):
-    #     super().__init__('mission_planner')
-    #     #maybe do a subscription creation loop for drone in drones[]
-    #     #self.declare_parameter("undetectedTags", ["tag36h11:52","tag36h11:203"]) 
-    #     #self.undetectedTags = set(self.get_parameter("undetectedTags"))
-    #     self.undetectedTags = {"tag36h11:200","tag36h11:204"}
-
-
-    #     self.subscription = self.create_subscription(
-    #         TFMessage,
-    #         'cf37/tf',
-    #         self.listener_callback,
-    #         10)
-    #     self.subscription2 = self.create_subscription(
-    #         TFMessage,
-    #         'cf38/tf',
-    #         self.listener_callback2,
-    #         10)
-    #     self.subscription3 = self.create_subscription(
-    #         TFMessage,
-    #         'cf39/tf',
-    #         self.listener_callback3,
-    #         10)
-    #     self.subscription  # prevent unused variable warning
-    #     self.subscription2  # prevent unused variable warning
-    #     self.subscription3
-
-
-    # def listener_callback(self, msg):
-    #     if msg.transforms:
-    #         detectedTag = msg.transforms[0].child_frame_id
-    #         self.get_logger().info('CF37 saw: "%s"' % detectedTag)
-    #         if detectedTag in self.undetectedTags:
-    #             self.get_logger().info('Target "%s" has been detected' % detectedTag)    
-    #             self.get_logger().info('Landing Crazyflie 37 on "%s"' % detectedTag)
-    #             self.undetectedTags.remove(detectedTag)
-    #             land_command(0x37)
-    #     return
-    
-    # def listener_callback2(self, msg):
-    #     if msg.transforms:
-    #         detectedTag = msg.transforms[0].child_frame_id
-    #         self.get_logger().info('CF38 saw: "%s"' % detectedTag)
-    #         if detectedTag in self.undetectedTags:
-    #             self.get_logger().info('Target "%s" has been detected' % detectedTag)    
-    #             self.get_logger().info('Landing Crazyflie 38 on "%s"' % detectedTag)       
-    #             self.undetectedTags.remove(detectedTag)
-    #             land_command(0x38)
-    #     return 
-
-    # def listener_callback3(self, msg):
-    #     if msg.transforms:
-    #         detectedTag = msg.transforms[0].child_frame_id
-    #         self.get_logger().info('CF39 saw: "%s"' % detectedTag)
-    #         if detectedTag in self.undetectedTags:
-    #             self.get_logger().info('Target "%s" has been detected' % detectedTag)    
-    #             self.get_logger().info('Landing Crazyflie 39 on "%s"' % detectedTag)       
-    #             self.undetectedTags.remove(detectedTag)
-    #             land_command(0x39)
-    #     return
 
 
 def main(args=None):
