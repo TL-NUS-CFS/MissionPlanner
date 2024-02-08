7af12f9004a3d54f6fc91633c7a12eb35ea9a6ae
swarm <liewyungjun@gmail.com>
2024-02-08 16:04:20 +0800

    Simultaneous takeoff and landing for drones of all channels by adding extra crazyradios

--- mission_planner/land_all.py
+++ mission_planner/land_all.py
@@ -4,44 +4,36 @@ import time
 import sys
 #from .land import land_command
 
-channels = [120, 80]
+channels = [120, 80] # Vary this for different channels 
 
 def land_all_command(drone_address=0xff):   
     cr = Crazyradio(devid=0) #devid = radio dongle id (0 being the first dongle)
+    cr.set_channel(channels[0])
+    cr.set_data_rate(cr.DR_2MPS)
 
-    for i in channels: 
-        cr.set_channel(i)
-        print(i)
-        cr.set_data_rate(cr.DR_2MPS)
-
-        for i in range(2):
-
-            # Send multicast packet to P2P port 7
-            cr.set_address((0xff,0xe7,0xe7,0xe7,0xe7)) # sets destination address for outgoing packets
-            cr.set_ack_enable(False) # disable acknowledgement for outgoing packets
-            cr.send_packet( (0xff, 0x80, 0x63, 0x00, drone_address) ) # sends packet to destination address via radio link 
-            print('send land to ' + str(drone_address))
-
-            time.sleep(0.01)
-        #cr.close()
-        #return 0
+    cr2 = Crazyradio(devid=1)
+    cr2.set_channel(channels[1])
+    cr2.set_data_rate(cr.DR_2MPS)
 
 
-# def land_all_command():
-#     channels = [80,120]
-#     crazyflie_data = {  
-#         "CF01": 80, "CF02": 80, "CF03": 80, "CF04": 80, "CF05": 80, "CF06": 120, "CF07": 120, "CF08": 120, "CF09": 100, "CF10": 100,
-#         "CF11": 120, "CF12": 120, "CF13": 120, "CF14": 120, "CF15": 120, "CF16": 60, "CF17": 120, "CF18": 60, "CF19": 60,
-#         "CF20": 60, "CF21": 60, "CF22": 60, "CF23": 60, "CF24": 60, "CF25": 60, "CF26": 90, "CF27": 90, "CF28": 90,
-#         "CF29": 90, "CF30": 90, "CF31": 110, "CF32": 110, "CF33": 110, "CF34": 110, "CF35": 110, "CF36": 70, "CF37": 70,
-#         "CF38": 70, "CF39": 70, "CF40": 70
-#     }
-    
+    for i in range(2):
 
-#     for drone_id,ch in crazyflie_data.items():
-#         numeric_part = int(drone_id[2:])
-#         hex_id = '0x' + str(numeric_part)
-#         land_command(channel=ch, drone_address=int(hex_id,16))
+        # Land all for 1st channel
+        cr.set_address((0xff,0xe7,0xe7,0xe7,0xe7)) # sets destination address for outgoing packets
+        cr.set_ack_enable(False) # disable acknowledgement for outgoing packets
+        cr.send_packet( (0xff, 0x80, 0x63, 0x00, drone_address) ) # sends packet to destination address via radio link 
+        print('CR1: send land to ' + str(drone_address))
+
+        # Land all for 1st channel
+        cr2.set_address((0xff,0xe7,0xe7,0xe7,0xe7)) 
+        cr2.set_ack_enable(False) 
+        cr2.send_packet( (0xff, 0x80, 0x63, 0x00, drone_address) ) 
+        print('CR2: send land to ' + str(drone_address))
+
+        time.sleep(0.01)
+    #cr.close()
+    #return 0
+
 
 
 if __name__ == '__main__':
--- mission_planner/takeoff.py
+++ mission_planner/takeoff.py
@@ -5,23 +5,32 @@ from land_all import land_all_command
 
 def takeoff(channel):
     cr = Crazyradio(devid=0)
-    cr.set_channel(channel)
+    print(channel)
+    cr.set_channel(int(channel[1]))
     cr.set_data_rate(cr.DR_2MPS)
 
+    cr2 = Crazyradio(devid=1)
+    cr2.set_channel(int(channel[2]))
+    cr2.set_data_rate(cr.DR_2MPS)
+
     for i in range(3):
-        # Send multicast packet to P2P port 7
+        # Takeoff for 1st channel 
         cr.set_address((0xff,0xe7,0xe7,0xe7,0xe7))
         cr.set_ack_enable(False)
         cr.send_packet( (0xff, 0x80, 0x63, 0x01, 0xff) )
-        print('send')
+        print('CR1: send')
+
+        # Takeoff for 2nd channel 
+        cr2.set_address((0xff,0xe7,0xe7,0xe7,0xe7))
+        cr2.set_ack_enable(False)
+        cr2.send_packet( (0xff, 0x80, 0x63, 0x01, 0xff) )
+        print('CR2: send')
 
         time.sleep(0.01)
     cr.close()
+    cr2.close()
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        for i in range(1, len(sys.argv)):
-            takeoff(channel = int(sys.argv[i]))
+    takeoff(channel=(sys.argv))
 
     timer_thread = threading.Timer(2, land_all_command) 
     timer_thread.start()       
-    #takeoff(channel=int(sys.argv[1]))
